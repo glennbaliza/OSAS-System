877af821c5757bd61cd0e5cd47d38df22d7e5197
glennbaliza <glenn.r.baliza20@gmail.com>
2020-10-27 02:03:37 +0800

    Base

--- OsasSystem/models.py
+++ OsasSystem/models.py
@@ -1,8 +1,19 @@
 from django.db import models
 from datetime import datetime
+import os, random
 from django.utils import timezone
+from django.utils.html import mark_safe
+
 
 now = timezone.now()
+
+def image_path(instance, filename):
+    basefilename, file_extension = os.path.splitext(filename)
+    chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
+    randomstr =''.join((random.choice(chars)) for x in range(5))
+    _now = datetime.now()
+
+    return 'profile_pic/{year}-{month}-{imageid}-{basename}-{randomstring}{ext}'.format(imageid = instance, basename = basefilename, randomstring=randomstr, ext=file_extension, year=_now.strftime('%Y'), month=_now.strftime('%m'), day=_now.strftime('%d')) 
 
 Status = ((0, 'Disable'),(1, 'Active'),)
 id_status = ((1, 'Approved'),(0, 'Pending'),)
@@ -40,8 +51,8 @@ class osas_r_personal_info(models.Model):
     stud_email = models.EmailField(max_length=50, verbose_name='Student Email')
     stud_hs = models.CharField(max_length=50, verbose_name='High School')
     stud_hs_add = models.CharField(max_length=50, verbose_name='High School Address')
-    date_created = models.DateField(auto_now=True)
-    date_updated = models.DateField(auto_now=True)
+    date_created = models.DateField(default=now)
+    date_updated = models.DateField(default=now)
     stud_course = models.ForeignKey('osas_r_course', on_delete=models.CASCADE)
     stud_sec_year = models.ForeignKey('osas_r_section_and_year', on_delete=models.CASCADE)
 
@@ -53,8 +64,8 @@ class osas_r_stud_contact(models.Model):
     stud_contact_id = models.IntegerField(primary_key=True)
     stud_m_number = models.BigIntegerField(blank='', verbose_name='Mobile Number')
     stud_t_number = models.BigIntegerField(blank='', verbose_name='Telephone Number')
-    date_created = models.DateField(timezone.now)
-    date_updated = models.DateField(timezone.now)
+    date_created = models.DateField(default=now)
+    date_updated = models.DateField(default=now)
     stud_id = models.ForeignKey('osas_r_personal_info', on_delete=models.CASCADE)
 
     def __str__(self):
@@ -67,8 +78,8 @@ class osas_r_emergency_contact(models.Model):
     stud_e_address = models.CharField(max_length=50, verbose_name='Emergency Contact Address')
     e_m_number = models.BigIntegerField(blank='', verbose_name='Mobile Number')
     e_t_number = models.BigIntegerField(blank='', verbose_name='Telephone Number')
-    date_created = models.DateField(timezone.now)
-    date_updated = models.DateField(timezone.now)
+    date_created = models.DateField(default=now)
+    date_updated = models.DateField(default=now)
     stud_id = models.ForeignKey('osas_r_personal_info', on_delete=models.CASCADE)
 
     def __str__(self):
@@ -77,8 +88,8 @@ class osas_r_emergency_contact(models.Model):
 class osas_t_id(models.Model):
 
     request_id = models.IntegerField(primary_key=True)
-    date_created = models.DateField(timezone.now)
-    date_updated = models.DateField(timezone.now)
+    date_created = models.DateField(default=now)
+    date_updated = models.DateField(default=now)
     lost_id_status = models.IntegerField(default=0, choices=id_status)
     lost_stud_id = models.ForeignKey('osas_r_personal_info', on_delete=models.CASCADE)
 
@@ -95,4 +106,36 @@ class osas_t_admission(models.Model):
 
     def __str__(self):
         return self.request_name, self.request_detail
+
+class osas_r_userrole(models.Model):
+    user_role = ((1,'Student'),(2,'Head'),(3,'Staff'))
+
+    user_id = models.IntegerField(primary_key=True)
+    user_name = models.CharField(max_length=50, verbose_name='Full Name')
+    user_username = models.CharField(max_length=50, verbose_name='User Name')
+    user_password = models.CharField(max_length=16, verbose_name='User Password')
+    user_email = models.EmailField(max_length=50, verbose_name='User Email')
+    user_type = models.IntegerField(max_length=50, choices=user_role, verbose_name='User Type')
+
+    def __str__(self):
+        return self.user_username
+
+class osas_r_stud_registration(models.Model):
+
+    student_id = models.IntegerField(primary_key=True)
+    s_fname = models.CharField(max_length=20)
+    s_lname = models.CharField(max_length=20)
+    s_no = models.CharField(max_length=13)
+    s_password = models.CharField(max_length=16)
+    date_created = models.DateField(timezone.now)
+    s_image = models.ImageField(upload_to=image_path, default='profile_pic/image.jpg')
+
+    def image_tag(self):
+        return mark_safe('<img src="/OsasSystem/media/%s" width="50" height="50" />'%(self.s_image))
+
+    def __str__(self):
+        return self.s_fname, self.s_no
+    
+
+
 
--- OsasSystem/urls.py
+++ OsasSystem/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.conf import settings
 from . import views
+from django.conf.urls.static import static
 
 app_name = 'OsasSystem'
 urlpatterns = [
-    path('', views.home, name="home"),
-]
+    path('', views.home, name="home") 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
